ac605a97e1580de36fa2fec1d6a7bdd463c3ba4a
Kyle Oba <koba@mudphone.com>
2019-05-21 00:42:25 -1000

    Reformat

--- test/pbt_test.exs
+++ test/pbt_test.exs
@@ -25,17 +25,11 @@ defmodule PbtTest do
     end
   end
 
-  def model_biggest(list) do
-    List.last(Enum.sort(list))
-  end
+  def model_biggest(list), do: List.last(Enum.sort(list))
   
-  def biggest([head | tail]) do
-    biggest(tail, head)
-  end
-
-  defp biggest([], max) do
-    max
-  end
+  def biggest([head | tail]), do: biggest(tail, head)
+  
+  defp biggest([], max), do: max
 
   defp biggest([head | tail], max) when head >= max do
     biggest(tail, head)
@@ -64,4 +58,56 @@ defmodule PbtTest do
   end
 
   defp increments(_, _), do: false
+
+  property "picks the last number" do
+    forall {list, known_last} <- {list(number()), number()} do
+      known_list = list ++ [known_last]
+      known_last == List.last(known_list)
+    end
+  end
+
+  property "a sorted list has ordered pairs" do
+    forall list <- list(term()) do
+      is_ordered(Enum.sort(list))
+    end
+  end
+
+  def is_ordered([a, b | t]) do
+    a <= b and is_ordered([b | t])
+  end
+
+  # lists with fewer than 2 elements
+  def is_ordered(_) do
+    true
+  end
+
+  property "a sorted list keeps its size" do
+    forall l <- list(number()) do
+      length(l) == length(Enum.sort(l))
+    end
+  end
+
+  property "no element added" do
+    forall l <- list(number()) do
+      sorted = Enum.sort(l)
+      Enum.all?(sorted, fn element -> element in l end)
+    end
+  end
+
+  property "no element deleted" do
+    forall l <- list(number()) do
+      sorted = Enum.sort(l)
+      Enum.all?(l, fn element -> element in sorted end)
+    end
+  end
+
+  property "symmetric encoding/decoding" do
+    forall data <- list({atom(), any()}) do
+      encoded = encode(data)
+      is_binary(encoded) and data == decode(encoded)
+    end
+  end
+
+  def encode(t), do: :erlang.term_to_binary(t)
+  def decode(t), do: :erlang.binary_to_term(t)
 end
