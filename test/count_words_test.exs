68e591883b1e25c31289381b746f22f707f9cc18
Kyle Oba <koba@mudphone.com>
2019-05-23 16:04:08 -1000

    Add generators

--- test/count_words_test.exs
+++ test/count_words_test.exs
@@ -3,22 +3,24 @@ defmodule CountWordsTest do
   use PropCheck
 
   property "count words" do
-    forall list_of_lists <- non_empty(list(non_empty(list(range(32, 126))))) do
+    forall list_of_lists <- non_empty(list(non_empty_ascii())) do
       space_matcher = ~r/\s+/
-
-      clean_word = fn char_list ->
-        s = to_string(char_list)
-        Regex.replace(space_matcher, s, "")
-      end
 
       words =
         list_of_lists
-        |> Enum.map(clean_word)
-        |> Enum.reject(&(&1 == ""))
+        |> Enum.map(&to_string/1)
+        |> Enum.reject(& &1 == "")
 
       num_words = length(words)
       sentence = Enum.join(words, " ")
       num_words == length(String.split(sentence, space_matcher))
     end
   end
+
+  def non_blank_ascii(), do: range(33, 126)
+  
+  def non_empty_ascii() do
+    let a <- non_empty(list(non_blank_ascii())), do: a
+  end
+
 end
